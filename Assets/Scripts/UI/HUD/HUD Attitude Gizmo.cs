9b67df3bde030b62b49b4a38c7c3909ed9628414
Penrose <parasolidox@gmail.com>
2024-08-22 01:36:09 +0300

    Added UI objects, new UI Volume

--- Assets/Scripts/UI/HUD/HUD Attitude Gizmo.cs
+++ Assets/Scripts/UI/HUD/HUD Attitude Gizmo.cs
@@ -4,13 +4,14 @@ public class HUDAttitudeGizmo : MonoBehaviour {
   public Transform target;
   public Transform display;
 
-  public Quaternion qTrim;
-
   private void LateUpdate () {
     Vector3 targetEA = target.rotation.eulerAngles;
-    
-    //targetEA.z = 0;
 
-    display.rotation = Quaternion.Euler ( targetEA ) * qTrim;
+    //targetEA.z = 0;
+    float change = targetEA.y;
+    targetEA.y = targetEA.z;
+    targetEA.z = change;
+
+    display.rotation = Quaternion.Euler ( targetEA );
   }
 }
