ea370d36d2df47c5962d40e0ae0bb61b4ec517ae
Penrose <parasolidox@gmail.com>
2024-08-23 01:41:24 +0300

    New Efficiency Band for Energy Core

--- Assets/Scripts/PUCKEnergy/EnergyCore.cs
+++ Assets/Scripts/PUCKEnergy/EnergyCore.cs
@@ -1,22 +1,22 @@
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnergyCore : MonoBehaviour {
-  public float maxEnergyDraw;
-  public float currentDraw;
-
-  public float overdrawStun;
-
   public List<EnergyUser> users;
-
   public float storedEnergy;
+
+  public float efficiencyHyperOffset;
+  public float efficiencyHypoOffset;
+
+  public float efficiencyHyperBonus;
+  public float efficiencyHypoBonus;
 
   public float mxStoredEnergyPlus;
   public float mxStoredEnergyMinus;
-
-  public float drawPercentage { get { return currentDraw / maxEnergyDraw; } }
+  
+  public float currentDraw { get; private set; }
+  public float drawPercentage { get { return ( storedEnergy - mxStoredEnergyMinus ) / ( mxStoredEnergyPlus - mxStoredEnergyMinus ); } }
 
   private void Awake () {
     users = new List<EnergyUser> ();
@@ -27,34 +27,33 @@ public class EnergyCore : MonoBehaviour {
     users.OrderBy ( u => Comparer<EnergyUser>.Create ( ( x, y ) => x.priority > y.priority ? 1 : x.priority < y.priority ? -1 : 0 ) );
   }
 
+  private bool safeguardHYPER = true;
+  private bool safeguardHYPO = true;
+
+  private void Start () {
+    if ( mxStoredEnergyPlus == efficiencyHyperOffset ) { safeguardHYPER = false; Debug.LogError ( "FUCKED HYPER EFFICIENCY PARAMTERS" ); }
+    if ( mxStoredEnergyMinus == efficiencyHyperOffset ) { safeguardHYPO = false; Debug.LogError ( "FUCKED HYPO EFFICIENCY PARAMTERS" ); }
+  }
+
   public void LateUpdate () {
     currentDraw = 0;
     for ( int i = 0; i < users.Count; i++ ) {
       currentDraw += users[i].draw;
-      users[i].drawEfficiency = 1;
+      storedEnergy -= users[i].draw * Time.deltaTime;
 
-      if ( currentDraw > maxEnergyDraw ) {
-        float overuse = currentDraw - maxEnergyDraw;
-
-        users[i].drawEfficiency = 1 - Mathf.Clamp ( storedEnergy / mxStoredEnergyMinus, 0, 1 ) - overuse / users[i].draw;
-        currentDraw = maxEnergyDraw;
-
-        storedEnergy -= overuse * Time.deltaTime;
-        if ( storedEnergy < mxStoredEnergyMinus ) {
-          storedEnergy = mxStoredEnergyMinus;
-        }
+      if ( storedEnergy < mxStoredEnergyMinus ) {
+        storedEnergy = mxStoredEnergyMinus;
+      }
+      if ( storedEnergy > mxStoredEnergyPlus ) {
+        storedEnergy = mxStoredEnergyPlus;
       }
 
-      if ( currentDraw < 0 ) {
-        float overuse = -currentDraw;
-
-        users[i].drawEfficiency = 1 + Mathf.Clamp ( storedEnergy / mxStoredEnergyPlus, 0, 1 ) + overuse / -users[i].draw;
-        currentDraw = 0;
-
-        storedEnergy += overuse * Time.deltaTime;
-        if ( storedEnergy > mxStoredEnergyPlus ) {
-          storedEnergy = mxStoredEnergyPlus;
-        }
+      users[i].drawEfficiency = 1;
+      if ( safeguardHYPER && storedEnergy > efficiencyHyperOffset ) {
+        users[i].drawEfficiency += efficiencyHyperBonus * ( storedEnergy - efficiencyHyperOffset ) / Mathf.Max( mxStoredEnergyPlus - efficiencyHyperOffset );
+      }
+      if ( safeguardHYPO && storedEnergy < efficiencyHypoOffset ) {
+        users[i].drawEfficiency += efficiencyHypoBonus * ( efficiencyHypoOffset - storedEnergy ) / Mathf.Max ( mxStoredEnergyMinus - efficiencyHypoOffset );
       }
     }
   }
--- Assets/Scripts/UI/HUD/HUDEnergyUse.cs
+++ Assets/Scripts/UI/HUD/HUDEnergyUse.cs
@@ -5,9 +5,22 @@ public class HUDEnergyUse : MonoBehaviour {
   public EnergyCore energyCore;
   public Slider slider;
 
+  public Slider drawPos;
+  public Slider drawNeg;
+
+  public GameObject handlePos;
+  public GameObject handleNeg;
+
   public float lerpStrength = 2;
 
   private void LateUpdate () {
-    slider.value = Mathf.Lerp ( slider.value, 1 + energyCore.drawPercentage, Time.deltaTime * lerpStrength );
+    
+    slider.value = Mathf.Lerp ( slider.value, energyCore.drawPercentage, Time.deltaTime * lerpStrength );
+
+    drawPos.value = Mathf.Lerp ( drawPos.value, energyCore.currentDraw < 0 ? -energyCore.currentDraw : 0, Time.deltaTime * lerpStrength );
+    drawNeg.value = Mathf.Lerp ( drawNeg.value, energyCore.currentDraw > 0 ? energyCore.currentDraw : 0, Time.deltaTime * lerpStrength );
+
+    handlePos.SetActive ( energyCore.currentDraw < 0 );
+    handleNeg.SetActive ( energyCore.currentDraw > 0 );
   }
 }
